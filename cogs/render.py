e3a8bc4df8f595472967220dc1e9a6ea607fd0b0
Daniel Melani <daniel.melani@gmail.com>
2023-04-08 06:49:20 +0200

    Add support for upscaling

--- cogs/render.py
+++ cogs/render.py
@@ -33,6 +33,17 @@ sampler_LUT = {
         "heun": ("Heun", 50)
         }
 
+dimensions_LUT = {
+        "square": (512, 512),
+        "landscape": (768, 512),
+        "portrait": (512, 768)
+        }
+
+upscalers_LUT = {
+        "normal": "R-ESRGAN 4x+",
+        "anime" : "R-ESRGAN 4x+ Anime6B"
+        }
+
 allowed_guilds = None
 disallowed_channels = ["general", "allmänt"]
 
@@ -72,7 +83,7 @@ class NonExitingArgumentParser(ArgumentParser):
         raise ArgParseException('%(prog)s: error: %(message)s\n' % args)
 
 class Txt2Img:
-    def __init__(self, prompt = "Dingle dot the test bot", negative_prompt = "", sampler_name="DPM++ SDE Karras", steps=30, filter_nsfw = True, batch_size=1, model=None, vae=None, width=512, height=512, clip_stop=1, restore_faces=False, cfg_scale=7):
+    def __init__(self, prompt = "Dingle dot the test bot", negative_prompt = "", sampler_name="DPM++ SDE Karras", steps=30, filter_nsfw = True, batch_size=1, model=None, vae=None, width=512, height=512, clip_stop=1, restore_faces=False, cfg_scale=7, upscaler=None):
         self.prompt = prompt
         self.negative_prompt = negative_prompt
         self.sampler_name = sampler_name
@@ -91,6 +102,12 @@ class Txt2Img:
             self.override_settings["sd_model_checkpoint"] = model
         if vae:
             self.override_settings["sd_vae"] = vae
+
+        if upscaler:
+            self.hr_upscaler = upscaler
+            self.enable_hr = True
+            self.hr_scale = 2
+            self.denoising_strength = 0.2
 
         self.override_settings_restore_afterwards = True
 
@@ -113,17 +130,13 @@ pics_args_parse.add_argument("-n", help="Number of pictures", default=1, type=in
 pics_args_parse.add_argument("--cfgs", help="Classifier Free Guidance Scale - how strongly the image should conform to prompt - lower values produce more creative results. Default is 7.", default=7, type=int)
 pics_args_parse.add_argument("-m", "--model", dest="data_model", help=f"Stable diffusion model", choices=models_LUT.keys(), default="deliberate", type=str)
 pics_args_parse.add_argument("-s", "--sampler", dest="sampler_name", help=f"Stable diffusion sampler", choices=sampler_LUT.keys(), default="dpmpp_sde_ka", type=str)
+pics_args_parse.add_argument("-i", dest="sampler_steps", help="Number of sampler steps", default=None, type=int)
 pics_args_parse.add_argument("-l", "--layout", dest="layout", default="square", choices=["square", "portrait", "landscape"])
 pics_args_parse.add_argument("--clip_stop", dest="clip_stop", help="Sets where to stop the CLIP language model. Default is 1. It works kinda like this in layers person -> male, female -> man, boy, woman girl -> and so on", default=1, choices=range(1, 5), type=int)
 pics_args_parse.add_argument("prompt", type=str)
 pics_args_parse.add_argument("neg_prompt", metavar="negative prompt", type=str, nargs='?', default="(bad quality, worst quality:1.4), child, kid, toddler")
 pics_args_parse.add_argument("--restore_faces", help="Attempts to restore faces", default=False, action='store_true')
-
-dimensions_LUT = {
-        "square": (512, 512),
-        "landscape": (768, 512),
-        "portrait": (512, 768)
-        }
+pics_args_parse.add_argument("-U", "--upscale", dest="upscaler", help=f"Upscale by 2x. Use with caution because it takes a lot of time to do.", default=None, choices=upscalers_LUT.keys())
 
 class Pics(commands.Cog):
     
@@ -202,6 +215,8 @@ class Pics(commands.Cog):
         restore_faces = args.restore_faces
         cfgs = args.cfgs
         sampler = args.sampler_name
+        upscaler = args.upscaler
+        sampler_steps = args.sampler_steps
 
         if filter_nsfw and "nsfw" not in neg_prompt:
             neg_prompt = "(nsfw:1.1), " + neg_prompt
@@ -209,7 +224,7 @@ class Pics(commands.Cog):
         if data_model is not None and data_model not in models_LUT:
             await ctx.send(f"Oi, {member}. No such model.")
             return
-        
+
         model = None
         vae = None
         if data_model:
@@ -219,10 +234,17 @@ class Pics(commands.Cog):
         steps = None
         if sampler:
             sampler_name, steps = sampler_LUT[sampler]
+
+        if sampler_steps:
+            steps = sampler_steps
+    
+        upscaler_name = None
+        if upscaler:
+            upscaler_name = upscalers_LUT[upscaler]
         
         await ctx.send(f"Ok, {member}. Rendering {prompt}")
 
-        t = Txt2Img(prompt=prompt, negative_prompt=neg_prompt, filter_nsfw=filter_nsfw, batch_size=batch_size, model=model, vae=vae, width=width, height=height, clip_stop=clip_stop, restore_faces=restore_faces, cfg_scale=cfgs, sampler_name=sampler_name, steps=steps)
+        t = Txt2Img(prompt=prompt, negative_prompt=neg_prompt, filter_nsfw=filter_nsfw, batch_size=batch_size, model=model, vae=vae, width=width, height=height, clip_stop=clip_stop, restore_faces=restore_faces, cfg_scale=cfgs, sampler_name=sampler_name, steps=steps, upscaler=upscaler_name)
         async with aiohttp.ClientSession() as session:
             async with session.post('http://192.168.1.43:7860/sdapi/v1/txt2img', data=t.to_json(), headers={'Content-type': 'application/json'}) as response:
                 r_data = await response.text()
